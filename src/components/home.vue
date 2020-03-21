395c5c3b6991fc34a53d27c0a475413c6180fe21
psiphonc <psiphonc@outlook.com>
2020-03-21 16:06:56 +0800

    home框架完成

--- src/components/home.vue
+++ src/components/home.vue
@@ -3,23 +3,33 @@
     <!-- 头部区域 -->
     <el-header height="80px">
       <div>
-        <img src="../assets/favicon.png" alt="logo" />
+        <img
+          src="../assets/favicon.png"
+          alt="logo"
+        />
         <span>后台管理系统</span>
       </div>
-      <el-button type="primary" class="logout-btn" @click="logout"
-        >退出</el-button
-      >
+      <el-button
+        type="primary"
+        class="logout-btn"
+        @click="logout"
+      >退出</el-button>
     </el-header>
     <!-- 页面主体区域 -->
     <el-container>
       <!-- 侧边栏 -->
       <el-aside :width="menuWidth">
-        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <div
+          class="toggle-button"
+          @click="toggleCollapse"
+        >|||</div>
         <!-- 侧边栏菜单区域 -->
         <el-menu
           unique-opened
           :collapse="isCollapse"
           :collapse-transition="false"
+          router
+          :default-active="'/'+activePath"
         >
           <!-- 一级菜单 -->
           <el-submenu
@@ -35,10 +45,12 @@
               <span>{{ menu.authName }}</span>
             </template>
             <!-- 二级菜单 -->
+            <!-- index绑定路径 el-menu已经开启了路由 会自动跳转到index绑定的路径 -->
             <el-menu-item
-              :index="subMenu.id + ''"
+              :index="'/' + subMenu.path + ''"
               v-for="subMenu in menu.children"
               :key="subMenu.id"
+              @click="saveNavState(subMenu.path)"
             >
               <!-- 二级菜单的模版区 -->
               <template slot="title">
@@ -52,7 +64,9 @@
         </el-menu>
       </el-aside>
       <!-- 主体内容 -->
-      <el-main>Main</el-main>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
@@ -60,27 +74,32 @@
 <script>
 export default {
   // 生命周期钩子函数
-  created: function() {
+  created: function () {
     this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePath')
   },
   methods: {
-    logout: function() {
+    logout: function () {
       // 清空token
       window.sessionStorage.clear()
       // 重定向到登陆页
       this.$router.push('login')
     },
-    getMenuList: async function() {
+    getMenuList: async function () {
       const res = await this.$http.get('menus')
-      console.log(res)
       if (res.status !== 200) return this.$message.error(res.meta.msg)
       this.menuList = res.data.data
     },
-    toggleCollapse: function() {
+    toggleCollapse: function () {
       this.isCollapse = !this.isCollapse
+    },
+    // 保存导航栏的激活状态
+    saveNavState: function (activePath) {
+      this.activePath = activePath
+      window.sessionStorage.setItem('activePath', activePath)
     }
   },
-  data: function() {
+  data: function () {
     return {
       // 左侧菜单栏
       menuList: [],
@@ -92,11 +111,13 @@ export default {
         145: 'iconfont icon-baobiao'
       },
       // 右侧菜单是否折叠
-      isCollapse: false
+      isCollapse: false,
+      // 被激活的导航项
+      activePath: ''
     }
   },
   computed: {
-    menuWidth: function() {
+    menuWidth: function () {
       if (this.isCollapse) return '64px'
       else return '200px'
     }
